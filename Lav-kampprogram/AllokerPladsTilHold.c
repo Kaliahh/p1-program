7f5e28adc7354af7c9b2644f8208ad7436b5c85a
Lena <durumguru@gmail.com>
2018-11-20 13:43:27 +0100

    Virker stadig ikke... Men lidt tættere på. Måske

--- Lav-kampprogram/AllokerPladsTilHold.c
+++ Lav-kampprogram/AllokerPladsTilHold.c
@@ -3,23 +3,23 @@
 #include <stdlib.h>
 #include "../main.h"
 
-void allocateSpaceForTeam(team *all_teams, int n);
+team* allocateSpaceForTeam(int n);
 
 int main (void) {
   int number_of_teams = 20, length = 0;
-  team *all_teams;
+  team *all_teams = allocateSpaceForTeam(number_of_teams);
 
-  allocateSpaceForTeam(all_teams, number_of_teams);
+  length = sizeof(all_teams)/sizeof(team);
 
-  /*length = sizeof(all_teams)/sizeof(team);
-
-  printf("%d\n", length);*/
+  printf("%d\n", length);
 
   free(all_teams);
 
   return 0;
 }
 
-void allocateSpaceForTeam (team *all_teams, int n) {
-  all_teams = (team*) calloc(n, sizeof(team));
+team* allocateSpaceForTeam (int n) {
+  team* all_teams = (team*) calloc(n, sizeof(team));
+
+  return all_teams;
 }
