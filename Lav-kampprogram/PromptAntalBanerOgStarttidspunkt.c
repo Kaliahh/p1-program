e624202e82fa6079eac2d5235fe5f21b440d9ca0
Unknown <jakob.soenderby@hotmail.com>
2018-11-23 10:55:29 +0100

    Lavet om til separate funktioner

--- Lav-kampprogram/PromptAntalBanerOgStarttidspunkt.c
+++ Lav-kampprogram/PromptAntalBanerOgStarttidspunkt.c
@@ -3,25 +3,30 @@
 #include <stdlib.h>
 #include "../main.h"
 
-void promptNumberOfFieldsAndStartingTime(int *number_of_fields, int *starting_time);
+int promptForFields();
+int promptForTime();
 
 int main(void) {
   int number_of_fields = 0, starting_time = 0;
 
-  promptNumberOfFieldsAndStartingTime(&number_of_fields, &starting_time);
+  number_of_fields = promptForFields();
+  starting_time = promptForTime();
 
   /* printf("Baner: %d, minutter: %d\n", number_of_fields, starting_time); */
-  
+
 }
 
-void promptNumberOfFieldsAndStartingTime(int *number_of_fields, int *starting_time) {
-  int minutter = 0, timer = 0;
-
+int promptForFields() {
+  int antal_baner = 0;
   printf("Indtast antal baner: ");
   scanf(" %d", number_of_fields);
+  return antal_baner;
+}
 
+int promptForTime() {
+  int minutter = 0, timer = 0;
   printf("Indtast starttidspunkt (skrevet som tt:mm): ");
   scanf(" %d:%d", &timer, &minutter);
 
-  *starting_time = timer * 60 + minutter;
+  return timer * 60 + minutter;
 }
