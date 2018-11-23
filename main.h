8e6f364ef667fa7137c0364da629f5da9f4889a3
Ane Søgaard Jørgensen <anesoejoe@gmail.com>
2018-11-23 10:26:58 +0100

    Tilføjet kommentarer og match struct

--- main.h
+++ main.h
@@ -1,9 +1,19 @@
 # define MAX_NAME_LEN 30
+# define GAMES_PR_TEAM 6
 
+/* Struct for et hold */
 typedef struct {
   char team[MAX_NAME_LEN];
   int games;
   int level;
 } team;
 
+/* Struct for en kamp */
+typedef struct{
+  char team_a[MAX_NAME_LEN];
+  char team_b[MAX_NAME_LEN];
+  int level;
+} match;
+
+/* Enumeration for niveauer */
 enum levels {N, A, B, C, EMPTY};
