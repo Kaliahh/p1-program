6711e6958054ac50d4a9bf8ddb73c2f4b489a113
Unknown <jakob.soenderby@hotmail.com>
2018-12-04 09:25:08 +0100

    Lavet mindre ændringer

--- Rediger-kampprogram/ScanForKampe.c
+++ Rediger-kampprogram/ScanForKampe.c
@@ -3,8 +3,8 @@
 # include <string.h>
 # include "../h-files/main.h"
 
-match *scanFileForMatches (FILE *fp, match *all_matches, int *number_of_matches);
-void getNumberOfMatches (FILE *fp, int *number_of_matches);
+match *scanFileForMatches (FILE *fp, match *all_matches, const int number_of_matches);
+int getNumberOfMatches (FILE *fp);
 
 int main(void) {
   int number_of_matches = 0, i = 0;
@@ -18,24 +18,24 @@ int main(void) {
      return -1;
   }
 
-  scanFileForMatches(fp, all_matches, &number_of_matches);
+  number_of_matches = getNumberOfMatches(fp);
+  scanFileForMatches(fp, all_matches, number_of_matches);
 
   for (i = 0; i < number_of_matches; i++) {
     printf("%s vs %s | %c\n", all_matches[i].team_a, all_matches[i].team_b, all_matches[i].level);
   }
 
   fclose (fp);
+  free(all_matches);
 
   return 0;
 }
 
-match *scanFileForMatches (FILE *fp, match *all_matches, int *number_of_matches) {
+match *scanFileForMatches (FILE *fp, match *all_matches, const int number_of_matches) {
   int scanres = 0, dump = 0, i = 0;
   char temp[200];
 
-  getNumberOfMatches(fp, number_of_matches);
-
-  all_matches = (match*) malloc (*number_of_matches * sizeof (match));
+  all_matches = (match*) malloc (number_of_matches * sizeof (match));
 
   while (feof(fp) == 0) {
     scanres = fscanf (fp, " %[-A-Za-z0-9| ]", temp);
@@ -52,15 +52,17 @@ match *scanFileForMatches (FILE *fp, match *all_matches, int *number_of_matches)
 }
 
 
-void getNumberOfMatches (FILE *fp, int *number_of_matches) {
+int getNumberOfMatches (FILE *fp) {
   char dump[5];
+  int number_of_matches = 0;
 
   while (feof(fp) == 0) {
     fgets (dump, 5, fp);
 
     if (strcmp(dump, "Bane") == 0) {
-      *number_of_matches += 1;
+      number_of_matches += 1;
     }
   }
-  printf("%d\n", *number_of_matches);
+  printf("%d\n", number_of_matches);
+  return number_of_matches;
 }
