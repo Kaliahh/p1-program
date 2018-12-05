e5a578f7dbb8314be80cb750c011d7b0dc43c46f
Lena <durumguru@gmail.com>
2018-12-05 13:52:40 +0100

    Laver et kampprogram, men med fejl.

--- Lav-kampprogram/ALGORITMEN-MVT.c
+++ Lav-kampprogram/ALGORITMEN-MVT.c
@@ -7,7 +7,8 @@
 # include <math.h>
 # include <time.h>
 # include "../h-files/main.h"
-# define NUMBER_OF_MATCHES 63
+# define NUMBER_OF_MATCHES 60
+# define CHECK_NUM 100000
 
 /* Struct for en kamp */
 typedef struct{
@@ -17,18 +18,21 @@ typedef struct{
   int field;
 } match_2;
 
-void createNewTournament(team *, int, match_2 *, int, int);
-int evaluateRound(match_2 *, int, int, int *);
-int is_already_in_round(match_2 *, int, int);
-int is_in_previous_round(match_2 *, int, int);
-int is_same_field(match_2 *, int, int);
+void copyTournament(const match_2 *, const int, match_2 *);
+int createNewTournament(team *, const int, match_2 *, const int, const int);
+int evaluateRound(const match_2 *, const int, const int, int *);
+int is_already_in_round(const match_2 *, const int, const int);
+int is_in_previous_round(const match_2 *, const int, const int);
+int is_same_field(const match_2 *, const int, const int);
 
 
 int main(void){
-  int number_of_teams = 21;
+  int i = 0;
+  int number_of_teams = 20;
   int number_of_fields = 2;
+  int no_go_count = 0;
   match_2 tournament[NUMBER_OF_MATCHES];
-  team all_teams[21];
+  team all_teams[20];
 
   time_t t;
   /* initialiserer rand */
@@ -54,7 +58,7 @@ int main(void){
   all_teams[17].level = 1;
   all_teams[18].level = 1;
   all_teams[19].level = 1;
-  all_teams[20].level = 1;
+ // all_teams[20].level = 1;
 
   all_teams[0].games = 0;
   all_teams[1].games = 0;
@@ -76,7 +80,7 @@ int main(void){
   all_teams[17].games = 0;
   all_teams[18].games = 0;
   all_teams[19].games = 0;
-  all_teams[20].games = 0;
+ // all_teams[20].games = 0;
 
   strcpy(all_teams[0].team, "Hold 1");
   strcpy(all_teams[1].team, "Hold 2");
@@ -98,25 +102,47 @@ int main(void){
   strcpy(all_teams[17].team, "Hold 18");
   strcpy(all_teams[18].team, "Hold 19");
   strcpy(all_teams[19].team, "Hold 20");
-  strcpy(all_teams[20].team, "Hold 21");
+  //strcpy(all_teams[20].team, "Hold 21");
 
-  createNewTournament(all_teams, number_of_teams, tournament, NUMBER_OF_MATCHES, number_of_fields);
+  no_go_count = createNewTournament(all_teams, number_of_teams, tournament, NUMBER_OF_MATCHES, number_of_fields);
+
+  for (i = 0; i < NUMBER_OF_MATCHES; i++) {
+    if (i % number_of_fields == 0 && i != 0) {
+      printf("- - - - - - - - - - - - - - - - - - - - - - - - -\n");
+      printf("%2d | %8s vs %-10s | %d\n", i + 1, tournament[i].team_a.team, tournament[i].team_b.team, tournament[i].field + 1);
+    }
+
+    else {
+      printf("%2d | %8s vs %-10s | %d\n", i + 1, tournament[i].team_a.team, tournament[i].team_b.team, tournament[i].field + 1);
+    }
+  }
+
+  printf("No_go_count: %d\n", no_go_count);
 
   return 0;
 }
 
 
-void createNewTournament(team *all_teams, int number_of_teams, match_2 *tournament, int number_of_matches, int number_of_fields){
-  int round_count = 0, tournament_index = 0, team_index = 0, sentinel = 0, sentinel_count = 0, grade = 0;
+int createNewTournament(team *all_teams, const int number_of_teams, match_2 *tournament, 
+                        const int number_of_matches, const int number_of_fields){
+  int i = 0;
+  int round_count = 0;
+  int tournament_index = 0;
+  int team_index = 0;
+  int sentinel = 0;
+  int sentinel_count = 0;
+  int sentinel_count_2 = 0;
+  int grade = 0;
+  int no_go_count = 0;
+  int team_a[number_of_fields];
+  int team_b[number_of_fields];
 
   for (round_count = 0; round_count < number_of_matches / number_of_fields; round_count++){
-    printf("r_i: %d\n", round_count);
-    for (tournament_index = round_count * number_of_fields; tournament_index < (round_count + 1) * number_of_fields; tournament_index++){
-      printf("T_I: %d\n", tournament_index);
+    for (i = 0, tournament_index = round_count * number_of_fields; tournament_index < (round_count + 1) * number_of_fields; tournament_index++){
 
-      sentinel = 0;
+      sentinel_count = 0;
 
-      while (sentinel == 0){
+      while (sentinel_count < CHECK_NUM){
         team_index = rand() % number_of_teams;
 
         if (all_teams[team_index].games <= 6 && all_teams[team_index].level != 4){
@@ -124,44 +150,67 @@ void createNewTournament(team *all_teams, int number_of_teams, match_2 *tourname
           tournament[tournament_index].team_a = all_teams[team_index];
           tournament[tournament_index].field = tournament_index % number_of_fields;
 
-          sentinel++;
+          sentinel_count = CHECK_NUM;
+          team_a[i] = team_index;
         }
+
+        sentinel_count++;
       }
 
-      sentinel = 0;
+      sentinel_count = 0;
 
-      while (sentinel == 0){            /* Sidder nogle gange fast her... */
-        team_index = rand() % number_of_teams;
+      while (sentinel_count < CHECK_NUM){            /* Sidder nogle gange fast her... */
+        team_index = (rand() + 1) % number_of_teams;
 
-        if (all_teams[team_index].games <= 6 && all_teams[team_index].level == tournament[tournament_index].team_a.level){
+        if (all_teams[team_index].games <= 6 && all_teams[team_index].level == tournament[tournament_index].team_a.level && strcmp(tournament[tournament_index].team_a.team, all_teams[team_index].team) != 0){
           all_teams[team_index].games++;
           tournament[tournament_index].team_b = all_teams[team_index];
 
-          sentinel++;
+          sentinel_count = CHECK_NUM;
+          team_b[i] = team_index;
+          i++;
         }
+
+        sentinel_count++;
       }
     }
 
-    if (evaluateRound(tournament, tournament_index, number_of_fields, &grade) > 0 && sentinel_count < 10){
+    no_go_count = evaluateRound(tournament, tournament_index, number_of_fields, &grade);
+
+    if (no_go_count > 0 && sentinel_count_2 != CHECK_NUM){
       round_count--;
-      sentinel_count++;
+      sentinel_count_2++;
+
+      for (i = 0; i < number_of_fields; i++){
+
+        all_teams[team_a[i]].games--;
+        all_teams[team_b[i]].games--;
+
+      }
+    }
+    else if (sentinel_count_2 == CHECK_NUM){
+      return no_go_count;
     }
   }
+
+
+  return no_go_count;
 }
 
-int evaluateRound(match_2 *tournament, int tournament_index, int number_of_fields, int *grade){
+int evaluateRound(const match_2 *tournament, const int tournament_index, const int number_of_fields, int *grade){
   int i = 0, no_go_count = 0;
+  int round_count = 0;
+
+  round_count = tournament_index / number_of_fields;
 
   for (i = tournament_index - number_of_fields; i < tournament_index; i++){
     if (is_already_in_round (tournament, i, number_of_fields) == 1){
       no_go_count++;
     }
-    if (is_in_previous_round (tournament, i, number_of_fields) == 1){
-      if (is_same_field (tournament, i, number_of_fields) == 0){
-        no_go_count++;
-      }
+    if (round_count != 0){
+      no_go_count += is_in_previous_round (tournament, i, number_of_fields);
     }
-    else {
+    else if (round_count != 0){
       *grade += 2;
     }
   }
@@ -169,7 +218,7 @@ int evaluateRound(match_2 *tournament, int tournament_index, int number_of_field
   return no_go_count;
 }
 
-int is_already_in_round(match_2 *tournament, int tournament_index, int number_of_fields){
+int is_already_in_round(const match_2 *tournament, const int tournament_index, const int number_of_fields){
   int i = 0, start_of_round = 0;
 
   start_of_round = tournament_index - (tournament_index % number_of_fields);
@@ -186,8 +235,10 @@ int is_already_in_round(match_2 *tournament, int tournament_index, int number_of
   return 0;
 }
 
-int is_in_previous_round(match_2 *tournament, int tournament_index, int number_of_fields) {
-  int i = 0, start_of_previous_round = 0;
+int is_in_previous_round(const match_2 *tournament, const int tournament_index, const int number_of_fields) {
+  int i = 0;
+  int no_go_count = 0;
+  int start_of_previous_round = 0;
 
   start_of_previous_round = tournament_index - ((tournament_index % number_of_fields) + number_of_fields);
 
@@ -196,21 +247,37 @@ int is_in_previous_round(match_2 *tournament, int tournament_index, int number_o
         strcmp(tournament[i].team_a.team, tournament[tournament_index].team_b.team) == 0 ||
         strcmp(tournament[i].team_b.team, tournament[tournament_index].team_a.team) == 0 ||
         strcmp(tournament[i].team_b.team, tournament[tournament_index].team_b.team) == 0) {
-      return 1;
+
+      if (is_same_field (tournament, i, number_of_fields) == 0){
+        no_go_count++;
+      }
     }
+
+    return no_go_count;
   }
 
   return 0;
 }
 
-int is_same_field(match_2 *tournament, int tournament_index, int number_of_fields){
-  if (tournament[tournament_index].field == tournament[tournament_index - number_of_fields].field) {
+int is_same_field(const match_2 *tournament, const int tournament_index, const int number_of_fields){
+  if (strcmp(tournament[tournament_index - number_of_fields].team_a.team, tournament[tournament_index].team_a.team) == 0 ||
+      strcmp(tournament[tournament_index - number_of_fields].team_a.team, tournament[tournament_index].team_b.team) == 0 ||
+      strcmp(tournament[tournament_index - number_of_fields].team_b.team, tournament[tournament_index].team_a.team) == 0 ||
+      strcmp(tournament[tournament_index - number_of_fields].team_b.team, tournament[tournament_index].team_b.team) == 0) {
+
     return 1;
   }
 
   return 0;
 }
 
+void copyTournament(const match_2 *temp_tournament, const int number_of_matches, match_2 *tournament) {
+  int tournament_index = 0;
+
+  for (tournament_index = 0; tournament_index < number_of_matches; tournament_index++) {
+    tournament[tournament_index] = temp_tournament[tournament_index];
+  }
+}
 
 
 
