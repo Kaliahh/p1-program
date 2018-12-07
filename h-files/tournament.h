83517ab77dfdb3f11e549d23900b60ed731e8d8c
Lena <durumguru@gmail.com>
2018-12-07 10:03:54 +0100

    Opdateret h-fil

--- h-files/tournament.h
+++ h-files/tournament.h
@@ -8,6 +8,9 @@ int createTournament(team *, const int, match *, const int, const int);
 int evaluateRound(const match *, const int, const int, int *);
 int is_already_in_round(const match *, const int, const int);
 int is_in_previous_round(const match *, const int, const int, int *);
-/*int is_different_match(const match *, const int, const int, const int);*/
+//int is_different_match(const match *, const int, const int, const int);
+void resetGames(match *tournament, team *, int, const int, const int);
 /*int played_in_a_row(const match *, const int, const int, int *);*/
 /*int number_of_times_played(const team *, const int);*/
+
+
