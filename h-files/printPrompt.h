35054a31ecfe72a58a121b85e9a6b7858b5d2f4b
Unknown <jakob.soenderby@hotmail.com>
2018-12-07 10:47:07 +0100

    Lavet isFileOpen, og fjernet printToTerminal
    Hvis der skal skrives til terminalen, skal file pointeren være stdout.

--- h-files/printPrompt.h
+++ h-files/printPrompt.h
@@ -1,8 +1,7 @@
 /* Prototyper */
 void createTemplate(void);
 void printTeams(const team *, const int);
-int printToFile(match *, int, int, int);
-void printToTerminal(match *, int, int, int);
+int printToFile(FILE *, match *, int, int, int);
 int printProgram(match *, int, int, int);
 char translateToChar(int);
 int getLevel(const char);
@@ -10,3 +9,4 @@ int promptForFields(void);
 int promptForTime(void);
 void promptForFileName(char *);
 void showPrintOptions(void);
+void isFileOpen(FILE *);
--- printPrompt.c
+++ printPrompt.c
@@ -6,12 +6,14 @@ void createTemplate(void) {
   FILE *fp = NULL;
 
   fp = fopen("holdnavne.txt", "w");
-
+  isFileOpen(fp);
   fprintf(fp, "%s\n", "# Holdnavn, NIVEAU");
 
   fclose(fp);
 }
 
+/* Printer alle hold til terminalen
+   Tager en pointer til et array af teams, og en int med antallet af elementer i arrayet */
 void printTeams(const team *all_teams, const int number_of_teams) {
   int team_index = 0;
   char level = ' ';
@@ -25,7 +27,9 @@ void printTeams(const team *all_teams, const int number_of_teams) {
   printf("\n");
 }
 
-/* Spørger brugeren om hvad der ønskes at gøre med turneringsplanen */
+/* Spørger brugeren om hvad der ønskes at gøre med turneringsplanen, og kalder relevante funktioner.
+   Parameterne er turneringen i form af en pointer til array af matches, en int med starttidspunkt for turneringen
+   en int med antallet af runder, og en int med antallet af baner */
 int printProgram(match *tournament, int starting_time, int number_of_rounds, int number_of_fields) {
   int choice = -1;
 
@@ -34,12 +38,15 @@ int printProgram(match *tournament, int starting_time, int number_of_rounds, int
     scanf(" %d", &choice);
 
     if (choice == 1) {
-      printToTerminal(tournament, starting_time, number_of_rounds, number_of_fields);
+      printToFile(stdout, tournament, starting_time, number_of_rounds, number_of_fields);
       showPrintOptions();
     }
 
     else if (choice == 2) {
-      printToFile(tournament, starting_time, number_of_rounds, number_of_fields);
+      FILE *fp;
+      fp = fopen("turneringsplan.txt", "w");
+      isFileOpen(fp);
+      printToFile(fp, tournament, starting_time, number_of_rounds, number_of_fields);
       showPrintOptions();
     }
 
@@ -55,27 +62,23 @@ int printProgram(match *tournament, int starting_time, int number_of_rounds, int
   return 0;
 }
 
-/* Viser valgmuligheder ved print */
+/* Viser valgmuligheder ved print i terminalen */
 void showPrintOptions(void) {
   printf("\n#####################    PRINT    #####################\n\n");
   printf("[1] Se kampprogram i terminalen \n[2] Gem kampprogram\n[0] Gå til hovedmenuen\n>> ");
 }
 
-/* Printer turneringsplan til fil */
-int printToFile(match *tournament, int starting_time, int number_of_rounds, int number_of_fields) {
+/* Printer turneringsplan til fil
+   Parameterne er en fil-pointer, enten filen der skal skrives til, eller stdout hvis der skal printes til terminalen,
+   en turnering i form af en pointer til array af matches, en int med starttidspunkt for turneringen
+   en int med antallet af runder, og en int med antallet af baner  */
+int printToFile(FILE *fp, match *tournament, int starting_time, int number_of_rounds, int number_of_fields) {
   int round_index = 0;
   int match_index = 0;
   int hour = 0;
   int minute = 0;
-  FILE *fp;
 
-  fp = fopen("turneringsplan.txt", "w");
-
-  if (fp == NULL) { /* Check at filen er NULL */
-    perror("Error opening file");
-    return -1;
-  }
-
+  /* Chekker om et givent index i turnerings arrayet indeholder gyldige hold. Afgjort ved at navnet starter med stort bogstav */
   while (isupper(tournament[match_index].team_a.team[0]) != 0 && isupper(tournament[match_index].team_b.team[0]) != 0) {
     hour = starting_time / 60;
     minute = starting_time % 60;
@@ -107,46 +110,7 @@ int printToFile(match *tournament, int starting_time, int number_of_rounds, int 
   return 0;
 }
 
-/* Printer turneringsplanen til terminalen */
-void printToTerminal(match *tournament, int starting_time, int number_of_rounds, int number_of_fields) {
-  int round_index = 0;
-  int match_index = 0;
-  int hour = 0;
-  int minute = 0;
-
-  printf("\n");
-
-  while (isupper(tournament[match_index].team_a.team[0]) != 0 && isupper(tournament[match_index].team_b.team[0]) != 0) {
-    hour = starting_time / 60;
-    minute = starting_time % 60;
-
-    /* Hvis det er den første kamp i runden */
-    if (match_index % number_of_fields == 0) {
-      /* Printer runde nummer og tidspunktet for hvornår der skal spilles */
-      printf("Runde %d:\n%.2d:%.2d\n", round_index + 1, hour, minute);
-      /* Printer banenummer, niveau og holdene der skal spille mod hinanden */
-      printf("Bane %2d | %c | %15s vs %-15s\n", tournament[match_index].field + 1, translateToChar(tournament[match_index].level),
-                                                tournament[match_index].team_a.team, tournament[match_index].team_b.team);
-      starting_time += ROUND_LEN;
-      round_index++;
-    }
-
-    /* Hvis det er den sidste kamp i runden */
-    else if (match_index % number_of_fields == number_of_fields - 1) {
-      printf("Bane %2d | %c | %15s vs %-15s\n", tournament[match_index].field + 1, translateToChar(tournament[match_index].level),
-                                                tournament[match_index].team_a.team, tournament[match_index].team_b.team);
-      printf("\n");
-    }
-
-    else {
-      printf("Bane %2d | %c | %15s vs %-15s\n", tournament[match_index].field + 1, translateToChar(tournament[match_index].level),
-                                                tournament[match_index].team_a.team, tournament[match_index].team_b.team);
-    }
-    match_index++;
-  }
-}
-
-/* Oversætter level fra tal til bogstaver */
+/* Oversætter level fra int til tilsvarende char */
 char translateToChar(int level) {
   switch(level) {
     case 0:
@@ -194,4 +158,13 @@ int promptForTime(void) {
 void promptForFileName(char *file_name) {
   printf("Indtast filnavn (afslut med .txt)\n>> ");
   scanf(" %s", file_name);
+}
+
+/* Undersøger om filen blev åbnet, hvis ikke skrives en fejl og programmet lukkes
+   Ellers sker der intet */
+void isFileOpen(FILE *fp) {
+  if(fp == NULL) {
+    perror("FEJL: Filen kunne ikke åbnes");
+    exit(EXIT_FAILURE);
+  }
 }
